94c9c7108986f477c618e7ad703ef62ae555cab5
Veiko Kääp <veikokaap@gmail.com>
2017-08-28 19:30:17 +0300

    Add ClassPrepareEvent and ThreadStartEvent

---
+++ src/main/java/kaap/veiko/debuggerforker/commands/sets/event/ClassPrepareEvent.java
@@ -0,0 +1,68 @@
+package kaap.veiko.debuggerforker.commands.sets.event;
+
+import kaap.veiko.debuggerforker.commands.constants.EventKindConstants;
+import kaap.veiko.debuggerforker.commands.parser.JDWPCommandContent;
+import kaap.veiko.debuggerforker.commands.types.ReferenceTypeID;
+import kaap.veiko.debuggerforker.commands.types.ThreadID;
+
+@JDWPCommandContent(id = EventKindConstants.CLASS_PREPARE)
+public class ClassPrepareEvent extends VirtualMachineEvent {
+    private final int requestId;
+    private final long thread;
+    private final byte refTypeTag;
+    private final long typeId;
+    private final String signature;
+    private final int status;
+
+    public ClassPrepareEvent(
+            int requestId,
+            ThreadID thread,
+            byte refTypeTag,
+            ReferenceTypeID typeID,
+            String signature,
+            int status
+    ) {
+        this.requestId = requestId;
+        this.thread = thread.asLong();
+        this.refTypeTag = refTypeTag;
+        this.typeId = typeID.asLong();
+        this.signature = signature;
+        this.status = status;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public long getThread() {
+        return thread;
+    }
+
+    public byte getRefTypeTag() {
+        return refTypeTag;
+    }
+
+    public long getTypeId() {
+        return typeId;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPrepareEvent{" +
+                "requestId=" + requestId +
+                ", thread=" + thread +
+                ", refTypeTag=" + refTypeTag +
+                ", typeId=" + typeId +
+                ", signature='" + signature + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
---
+++ src/main/java/kaap/veiko/debuggerforker/commands/sets/event/ThreadStartEvent.java
@@ -0,0 +1,35 @@
+package kaap.veiko.debuggerforker.commands.sets.event;
+
+import kaap.veiko.debuggerforker.commands.constants.EventKindConstants;
+import kaap.veiko.debuggerforker.commands.parser.JDWPCommandContent;
+import kaap.veiko.debuggerforker.commands.types.ThreadID;
+
+@JDWPCommandContent(id = EventKindConstants.THREAD_START)
+public class ThreadStartEvent extends VirtualMachineEvent {
+    private final int requestId;
+    private final long thread;
+
+    public ThreadStartEvent(
+            int requestId,
+            ThreadID thread
+    ) {
+        this.requestId = requestId;
+        this.thread = thread.asLong();
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public long getThread() {
+        return thread;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreadStartEvent{" +
+                "requestId=" + requestId +
+                ", thread=" + thread +
+                '}';
+    }
+}
