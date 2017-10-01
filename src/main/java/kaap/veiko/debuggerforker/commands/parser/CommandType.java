f51043d578390beeedf199dd530c5835f0e1d433
Veiko Kääp <veikokaap@gmail.com>
2017-10-01 19:30:54 +0300

    refactor Remove unneeded and confusing CommandType.EITHER

--- src/main/java/kaap/veiko/debuggerforker/commands/parser/CommandType.java
+++ src/main/java/kaap/veiko/debuggerforker/commands/parser/CommandType.java
@@ -1,15 +1,11 @@
 package kaap.veiko.debuggerforker.commands.parser;
 
 public enum CommandType {
-  EITHER,
   REPLY,
   COMMAND;
 
   public boolean check(boolean reply) {
-    if (this == EITHER) {
-      return true;
-    }
-    else if (this == COMMAND) {
+    if (this == COMMAND) {
       return !reply;
     }
     else {
