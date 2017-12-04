f8591b3c5bcc95f94e205a58f812e36149a7f151
Veiko Kääp <veikokaap@gmail.com>
2017-12-04 19:36:30 +0200

    Code cleanup

--- connections/src/main/java/kaap/veiko/debuggerforker/connections/VirtualMachineManager.java
+++ connections/src/main/java/kaap/veiko/debuggerforker/connections/VirtualMachineManager.java
@@ -5,16 +5,13 @@ import java.nio.channels.SocketChannel;
 
 import kaap.veiko.debuggerforker.packet.PacketStream;
 import kaap.veiko.debuggerforker.packet.VirtualMachinePacketStream;
-import kaap.veiko.debuggerforker.types.VMInformation;
 
 public class VirtualMachineManager implements Connection {
 
   private final PacketStream packetStream;
-  private final VMInformation vmInformation;
 
   public VirtualMachineManager(SocketChannel socketChannel) throws IOException {
     this.packetStream = new VirtualMachinePacketStream(socketChannel);
-    this.vmInformation = new VMInformation();
   }
 
   public PacketStream getPacketStream() {
@@ -25,14 +22,4 @@ public class VirtualMachineManager implements Connection {
   public void close() throws IOException {
     packetStream.close();
   }
-
-//  public void removeBreakPoint(int requestId)  {
-//    Packet command = new ClearEventRequestCommand(EventKind.BREAKPOINT, requestId), false);
-//    try {
-//      packetStream.write(command);
-//    }
-//    catch (IOException e) {
-//      e.printStackTrace();
-//    }
-//  }
 }
