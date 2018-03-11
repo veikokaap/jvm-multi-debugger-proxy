d302e85a857266c1659ae42db54a0f98bd6b986d
Veiko Kääp <veikokaap@gmail.com>
2018-03-11 17:50:05 +0200

    Small improvements

--- integration-tests/src/test/kotlin/ee/veikokaap/debugproxy/tests/testFunctions.kt
+++ integration-tests/src/test/kotlin/ee/veikokaap/debugproxy/tests/testFunctions.kt
@@ -2,12 +2,14 @@ package ee.veikokaap.debugproxy.tests
 
 import ee.veikokaap.debugproxy.testframework.DebuggerProcess
 import ee.veikokaap.debugproxy.testframework.JvmProcess
+import org.hamcrest.Matchers
+import org.junit.Assert
 import java.util.*
 import kotlin.test.assertEquals
 
 fun Deque<String>.assertContainsOnly(vararg lines: String) {
     lines.forEach{line -> assertEquals(line, this.pollFirst())}
-    assertEquals(null, this.pollFirst())
+    Assert.assertThat(this, Matchers.`is`(Matchers.empty()))
 }
 
 inline fun runTest(testClass: Class<*>, test: (JvmProcess, DebuggerProcess) -> Unit) {
