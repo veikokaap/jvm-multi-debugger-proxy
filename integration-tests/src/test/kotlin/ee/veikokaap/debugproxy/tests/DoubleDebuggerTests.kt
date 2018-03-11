d5ac08c8683f0578a3e983db32afe7fe361cf1db
Veiko Kääp <veikokaap@gmail.com>
2018-03-11 17:49:50 +0200

    Add test

--- integration-tests/src/test/kotlin/ee/veikokaap/debugproxy/tests/DoubleDebuggerTests.kt
+++ integration-tests/src/test/kotlin/ee/veikokaap/debugproxy/tests/DoubleDebuggerTests.kt
@@ -1,8 +1,12 @@
 package ee.veikokaap.debugproxy.tests
 
 import ee.veikokaap.debugproxy.testframework.utils.BreakpointUtil
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.empty
+import org.junit.Assert.assertThat
 import org.junit.Test
 import java.util.concurrent.TimeUnit
+import kotlin.test.assertTrue
 
 open class DoubleDebuggerTests {
 
@@ -10,7 +14,31 @@ open class DoubleDebuggerTests {
     val firstLocation = BreakpointUtil.findBreakLocation(testClass, 0)
     val secondLocation = BreakpointUtil.findBreakLocation(testClass, 1)
 
-    @Test(timeout = 5000)
+    @Test(timeout = 10000)
+    fun `test single breakpoint with 2 debuggers`() = runTest(testClass) { jvm, firstDebugger, secondDebugger ->
+        jvm.outputDeque.assertContainsOnly("Listening for transport dt_socket at address: 16789")
+
+        val firstBreak = firstDebugger.breakAt(firstLocation) {
+            jvm.outputDeque.assertContainsOnly("Before breakpoints")
+            it.resume()
+        }
+
+        val secondBreak = secondDebugger.breakAt(firstLocation) {
+            firstBreak.joinAndTest(4, TimeUnit.SECONDS)
+            assertThat(jvm.outputDeque, `is`(empty()))
+            assertTrue(jvm.outputDeque.isEmpty())
+            it.resume()
+        }
+
+        firstDebugger.allBreakpointSet()
+
+        secondBreak.joinAndTest(4, TimeUnit.SECONDS)
+        jvm.waitForExit(4, TimeUnit.SECONDS)
+
+        jvm.outputDeque.assertContainsOnly("After breakpoint 0", "After breakpoint 1")
+    }
+
+    @Test(timeout = 10000)
     fun `test 2 breakpoints with 2 debuggers`() = runTest(testClass) { jvm, firstDebugger, secondDebugger ->
         jvm.outputDeque.assertContainsOnly("Listening for transport dt_socket at address: 16789")
 
@@ -26,9 +54,9 @@ open class DoubleDebuggerTests {
 
         firstDebugger.allBreakpointSet()
 
-        firstBreak.joinAndTest(1, TimeUnit.SECONDS)
-        secondBreak.joinAndTest(1, TimeUnit.SECONDS)
-        jvm.waitForExit(1, TimeUnit.SECONDS)
+        firstBreak.joinAndTest(4, TimeUnit.SECONDS)
+        secondBreak.joinAndTest(4, TimeUnit.SECONDS)
+        jvm.waitForExit(4, TimeUnit.SECONDS)
 
         jvm.outputDeque.assertContainsOnly("After breakpoint 1")
     }
