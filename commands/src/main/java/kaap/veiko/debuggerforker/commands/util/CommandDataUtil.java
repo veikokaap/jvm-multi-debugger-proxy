a24765b5a40cd6930fdcc861d0242b99664ca334
Veiko Kääp <veikokaap@gmail.com>
2018-06-17 17:38:15 +0300

    Increase ByteBuffer size and try again when command writing fails due to BufferOverflowException

--- commands/src/main/java/kaap/veiko/debuggerforker/commands/util/CommandDataUtil.java
+++ commands/src/main/java/kaap/veiko/debuggerforker/commands/util/CommandDataUtil.java
@@ -1,22 +1,38 @@
 package kaap.veiko.debuggerforker.commands.util;
 
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 
 import kaap.veiko.debuggerforker.commands.Command;
 import kaap.veiko.debuggerforker.packet.internal.ByteBufferDataWriter;
-import kaap.veiko.debuggerforker.types.DataWriter;
 import kaap.veiko.debuggerforker.types.VMInformation;
 
 public class CommandDataUtil {
   public static byte[] getCommandDataBytes(Command command, VMInformation vmInformation) {
-    ByteBuffer buffer = ByteBuffer.allocate(1024);
-    DataWriter writer = new ByteBufferDataWriter(buffer, vmInformation);
-    command.writeCommand(writer);
+    int bufferSize = 1024;
+
+    ByteBuffer buffer = null;
+    while (buffer == null) {
+      try {
+        buffer = writeCommandBytesToBuffer(command, vmInformation, bufferSize);
+      }
+      catch (BufferOverflowException e) {
+        // Buffer not big enough, double its size and try again
+        bufferSize *= 2;
+        buffer = null;
+      }
+    }
 
     byte[] bytes = new byte[buffer.position()];
     buffer.flip();
     buffer.get(bytes);
 
     return bytes;
+  }
+
+  private static ByteBuffer writeCommandBytesToBuffer(Command command, VMInformation vmInformation, int bufferSize) {
+    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+    command.writeCommand(new ByteBufferDataWriter(buffer, vmInformation));
+    return buffer;
   }
 }
